cbfdda2b6c94a01affbbcee2dcb868d4719b284f
alexandrugatea <alex@digitalrepublic.ro>
2019-03-25 13:51:52 +0200

    bug fixing for js animations and other css fixes

--- js/telenav.js
+++ js/telenav.js
@@ -2,8 +2,6 @@ jQuery(document).ready(function($){
     
     // check window width
     var wW = $(window).width();
-    // check window height
-    var wH = $(window).height();
 
     // and only add animation effects if the viewport is a tablet or higher 
     // visibility is dependent of css
@@ -14,26 +12,36 @@ jQuery(document).ready(function($){
                 $(this).addClass('animated fadeInUp')},
                 tolerance: 50
         });
-
     }
 
     // switch between prototype and concept
-
     var switcher = $('[data-switch]');
-
     switcher.on('click', function(){
         $(this).toggleClass('concept prototype btn-primary btn-accent');
         $("#app").toggleClass('inactive');
         $("#concept").toggleClass('active');
     });
 
-
+    // init the floating button from the bottom of the page
     $('.floating-dial-top-center button').float(0, 0, 90, 90, 225, 315, 1, 'fa-link', 'fa-unlink');
 
 
-    // resolution check 
-    if (wW < 1366 && wH < 768) {
-        $('.modal').css('display', 'flex');
-    }
+    // init the slick carousel inside the concept
 
+    var carousel = $('.concept-carousel');
+
+    carousel.slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        infinite: false,
+        autoPlay: false,
+        dots: true
+    });
+
+    carousel.on('afterChange', function(event, slick, currentSlide) {
+        if (slick.$slides.length - 1 == currentSlide) {
+            $('.splash').find('.btn-telenav').addClass('animated slideInUp');
+        }
+      })
 });
