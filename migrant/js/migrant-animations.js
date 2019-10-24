24b2f67e6f5086cdc1eb309702d6e599987c79d2
alexandrugatea <alexandru.gatea@gmail.com>
2019-10-25 00:58:51 +0300

    added links to forms

--- migrant/js/migrant-animations.js
+++ migrant/js/migrant-animations.js
@@ -20,7 +20,7 @@ disabledLink.on('click', function(e){
 
 // move to next screen, while animating
 
-var option = $('.option-link:not(.disabled)');
+var option = $('.choose .option-link:not(.disabled)');
 
 option.on('click', function( ){
     $(this).parents('.options.active').addClass('is-transitioning');
