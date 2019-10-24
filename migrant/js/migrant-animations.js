69875554817f61b52148ac81dbb596fa23b5f08a
alexandrugatea <alexandru.gatea@gmail.com>
2019-10-25 00:54:12 +0300

    added assets and the first 2 screens. tryed an animation

---
+++ migrant/js/migrant-animations.js
@@ -0,0 +1,36 @@
+jQuery(document).ready(function($){
+
+
+// animate tap on disabled optionlink
+
+const disabledLink            = $('.option-link.disabled');
+const disabledAnimatedElement = $('.option-info');
+
+disabledLink.on('click', function(e){
+    e.preventDefault();
+    $(this).css('pointer-events', 'none');
+    disabledAnimatedElement.addClass('animated heartBeat');
+
+    setTimeout(function(){
+        disabledLink.css('pointer-events', 'all');
+        disabledAnimatedElement.removeClass('heartBeat');
+    }, 1000)
+});
+
+
+// move to next screen, while animating
+
+var option = $('.option-link:not(.disabled)');
+
+option.on('click', function( ){
+    $(this).parents('.options.active').addClass('is-transitioning');
+    setTimeout(function(){
+        $('.options.choose').removeClass('active');
+        $('.options.send-receive').addClass('active');
+    }, 600);
+});
+
+
+
+
+});
