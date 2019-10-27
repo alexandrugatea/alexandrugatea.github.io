53894896a22b72f76f9acf306e8d08f96d293836
alexandrugatea <alexandru.gatea@gmail.com>
2019-10-27 21:04:33 +0200

    changed homepage, added sender form and animation

--- migrant/js/migrant-animations.js
+++ migrant/js/migrant-animations.js
@@ -1,36 +1,46 @@
-jQuery(document).ready(function($){
+jQuery(document).ready(function ($) {
 
 
-// animate tap on disabled optionlink
+    // animate tap on disabled optionlink
 
-const disabledLink            = $('.option-link.disabled');
-const disabledAnimatedElement = $('.option-info');
+    const disabledLink = $('.option-link.disabled');
+    const disabledAnimatedElement = $('.option-info');
 
-disabledLink.on('click', function(e){
-    e.preventDefault();
-    $(this).css('pointer-events', 'none');
-    disabledAnimatedElement.addClass('animated heartBeat');
+    disabledLink.on('click', function (e) {
+        e.preventDefault();
+        $(this).css('pointer-events', 'none');
+        disabledAnimatedElement.addClass('animated heartBeat');
 
-    setTimeout(function(){
-        disabledLink.css('pointer-events', 'all');
-        disabledAnimatedElement.removeClass('heartBeat');
-    }, 1000)
-});
-
-
-// move to next screen, while animating
-
-var option = $('.options.choose .option-link:not(.disabled)');
-
-option.on('click', function( ){
-    $(this).parents('.options.active').addClass('is-transitioning');
-    setTimeout(function(){
-        $('.options.choose').removeClass('active');
-        $('.options.send-receive').addClass('active');
-    }, 600);
-});
+        setTimeout(function () {
+            disabledLink.css('pointer-events', 'all');
+            disabledAnimatedElement.removeClass('heartBeat');
+        }, 1000)
+    });
 
 
+    // move to next screen, while animating
 
+    var option = $('.options.choose .option-link:not(.disabled)');
+
+    option.on('click', function () {
+        $(this).parents('.home').addClass('switched');
+    });
+
+    // animate inputs on page load
+
+    $('.carousel-item').each(function(){
+        var delay = 100;
+        $(this).find('.is-animated').each(function(){
+            $(this).css('animation-delay', delay + 'ms');
+            delay += 100;
+        });
+    });
+
+
+    $('.carousel-item.active .is-animated').addClass('animated fadeInUp');
+
+    $('.carousel').on('slid.bs.carousel', function(){
+        $('.carousel-item.active .is-animated').addClass('animated fadeInUp');
+    })
 
 });
