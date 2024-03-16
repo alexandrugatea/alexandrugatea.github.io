14b4cccee5614517a05235c604e7f9fea57edd85
Alexandru Gatea <alexandru.gatea@gmail.com>
2024-03-16 14:37:34 +0200

    added cookiebot

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -582,3 +582,26 @@ function closeMenu() {
     navList.classList.remove("open");
     navTrig.classList.remove("open");
 }
+
+// cookie banner
+var cookieBannerSliderPos = 0;
+
+function showCookieBanner() {
+    var cookiebanner = document.getElementById("cookiebanner");
+    var dialogHeight = parseInt(cookiebanner.offsetHeight);
+    cookiebanner.style.bottom = (cookieBannerSliderPos - dialogHeight) + "px";
+    cookieBannerSliderPos += 4;
+    if (cookieBannerSliderPos < dialogHeight) {
+        setTimeout(function () {
+            showCookieBanner();
+        }, 1);
+    } else {
+        cookieBannerSliderPos = 0;
+        cookiebanner.style.bottom = "0px";
+    }
+}
+
+function hideCookieBanner() {
+    var cookiebanner = document.getElementById("cookiebanner");
+    cookiebanner.style.display = "none";
+}
