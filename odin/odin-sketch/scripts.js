022cb7d0ae5f761e104dc072f637e30e13cbc48e
Alexandru Gatea <alexandru.gatea@gmail.com>
2024-03-22 18:05:26 +0200

    [TOC] [Sketch Project] refactored JS, changed meta information

--- odin/odin-sketch/scripts.js
+++ odin/odin-sketch/scripts.js
@@ -1,31 +1,31 @@
-// Create the grid
 
-// get the grid size from input
 let gridSize = document.getElementById("gridSize").value;
-
-// Create squares
-
 let gridContainer = document.getElementById("grid");
-
-
 let currentGridSize = document.getElementById("currentGridSize");
-updateCurrentGridSize(gridSize);
-
-
-// update the value inside Apply button
 let selectedSize = document.getElementById("selectedSize");
 let rangeSlider = document.getElementById("gridSize");
+let applySize = document.getElementById("changeGrid");
+let drawingColor = document.querySelector(".color-list .selected").getAttribute("data-color");
+let availableColors = document.querySelectorAll(".color-list li button.color");
+let currentBackgroundColor = document.querySelector(".background-list-item button.background.selected").getAttribute("data-color");
+let bgBtn = document.querySelectorAll("button.background");
+let showGuides = document.querySelector("#showGuides");
+let clearDrawing = document.getElementById("clearDrawing");
 
+
+// Init functions
+updateCurrentGridSize(gridSize);
+generateGrid(gridSize);
+drawOnDrag();
+changeBackgroundColor(currentBackgroundColor);
+
+
+// Event Listeners
 rangeSlider.addEventListener("input", function(){
     gridSize = document.getElementById("gridSize").value;
     selectedSize.innerText = gridSize + " x " + gridSize
 });
 
-generateGrid(gridSize);
-drawOnDrag();
-
-
-let applySize = document.getElementById("changeGrid");
 
 applySize.addEventListener("click", function() {
     gridSize = document.getElementById("gridSize").value;
@@ -35,30 +35,7 @@ applySize.addEventListener("click", function() {
     drawOnDrag();
 });
 
-
-function generateGrid (gridSize) {
-    gridContainer.style.setProperty('--cells', gridSize);
-    
-    for (let i = 1; i<=gridSize; i++) {
-        for (let j = 1; j<=gridSize; j++) {
-            const gridCell = document.createElement("div");
-            gridCell.classList.add("grid-cell", 'cell-index-' + i + "-" + j);
-            gridContainer.appendChild(gridCell);
-        }
-    }
-}
-
-function destroyGrid() {
-    gridContainer.innerHTML= "";
-}
-
-let drawingColor = document.querySelector(".color-list .selected").getAttribute("data-color");
-console.log(drawingColor);
-
-let availableColors = document.querySelectorAll(".color-list li button.color");
-
 availableColors.forEach(function(color) {
-
     color.addEventListener("click", function() {
         let newColor = this.getAttribute("data-color");
         drawingColor = newColor;
@@ -67,6 +44,66 @@ availableColors.forEach(function(color) {
 
 });
 
+document.getElementById('exportDrawing').addEventListener('click', exportDrawing);
+
+bgBtn.forEach(function(item) {
+    item.addEventListener("click", function() {
+        let newBackgroundColor = this.getAttribute("data-color");
+        changeBackgroundColor(newBackgroundColor);
+        updateActiveItem(document.querySelector(".background-list"),item)
+    });
+});
+
+
+clearDrawing.addEventListener("click", clearBoard);
+
+showGuides.addEventListener("click", function() {
+    gridContainer.classList.toggle("no-guides");
+});
+
+
+// Functions
+function clearBoard () {
+    let boardCell = document.querySelectorAll(".grid-cell");
+
+    boardCell.forEach(function(cell) {
+        cell.style = "";
+    });
+}
+
+function updateActiveItem(parent, activeItem) {
+    let container = parent;
+    let bgListItems = container.querySelectorAll('button');
+    bgListItems.forEach(function (bg) {
+        bg.classList.remove('selected');
+    });
+    activeItem.classList.add('selected');
+}
+
+function changeBackgroundColor(color) {
+    gridContainer.style.backgroundColor = color;
+}
+
+function exportDrawing() {
+    const now = new Date();
+    const year = now.getFullYear().toString();
+    const month = (now.getMonth() + 1).toString().padStart(2, '0'); 
+    const day = now.getDate().toString().padStart(2, '0');
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+    const filename = `AG_PixelAvatar_${year}${month}${day}${hours}${minutes}${seconds}.png`;
+
+    html2canvas(gridContainer, {backgroundColor:null}).then(canvas => {
+        // Create an image from the canvas
+        let image = canvas.toDataURL("image/png");
+        // Create a link to download the image
+        let link = document.createElement('a');
+        link.download = filename;
+        link.href = image;
+        link.click();
+    });
+}
 
 function drawOnDrag() {
     let isDrawing = false;
@@ -115,76 +152,18 @@ function newGridSize(size) {
     currentGridSize.innerText = size + "px * " + size + "px";
 }
 
-document.getElementById('exportDrawing').addEventListener('click', function() {
-    const now = new Date();
-    const year = now.getFullYear().toString();
-    const month = (now.getMonth() + 1).toString().padStart(2, '0'); 
-    const day = now.getDate().toString().padStart(2, '0');
-    const hours = now.getHours().toString().padStart(2, '0');
-    const minutes = now.getMinutes().toString().padStart(2, '0');
-    const seconds = now.getSeconds().toString().padStart(2, '0');
-    const filename = `AG_PixelAvatar_${year}${month}${day}${hours}${minutes}${seconds}.png`;
-
-    html2canvas(gridContainer, {backgroundColor:null}).then(canvas => {
-        // Create an image from the canvas
-        let image = canvas.toDataURL("image/png");
-        // Create a link to download the image
-        let link = document.createElement('a');
-        link.download = filename;
-        link.href = image;
-        link.click();
-    });
-});
-
-
-
-let currentBackgroundColor = document.querySelector(".background-list-item button.background.selected").getAttribute("data-color");
-if (currentBackgroundColor === "transparent") {currentBackgroundColor = ""}
-
-changeBackgroundColor(currentBackgroundColor);
-
-
-function changeBackgroundColor(color) {
-    gridContainer.style.backgroundColor = color;
-}
-
-let bgBtn = document.querySelectorAll("button.background");
-
-bgBtn.forEach(function(item) {
+function generateGrid (gridSize) {
+    gridContainer.style.setProperty('--cells', gridSize);
     
-
-    item.addEventListener("click", function() {
-        let newBackgroundColor = this.getAttribute("data-color");
-        changeBackgroundColor(newBackgroundColor);
-        updateActiveItem(document.querySelector(".background-list"),item)
-    });
-});
-
-
-function updateActiveItem(parent, activeItem) {
-    let container = parent;
-    let bgListItems = container.querySelectorAll('button');
-    bgListItems.forEach(function (bg) {
-        bg.classList.remove('selected');
-    });
-    activeItem.classList.add('selected');
+    for (let i = 1; i<=gridSize; i++) {
+        for (let j = 1; j<=gridSize; j++) {
+            const gridCell = document.createElement("div");
+            gridCell.classList.add("grid-cell", 'cell-index-' + i + "-" + j);
+            gridContainer.appendChild(gridCell);
+        }
+    }
 }
 
-
-let clearDrawing = document.getElementById("clearDrawing");
-
-clearDrawing.addEventListener("click", clearBoard);
-
-function clearBoard () {
-    let boardCell = document.querySelectorAll(".grid-cell");
-
-    boardCell.forEach(function(cell) {
-        cell.style = "";
-    });
+function destroyGrid() {
+    gridContainer.innerHTML= "";
 }
-
-let showGuides = document.querySelector("#showGuides");
-
-showGuides.addEventListener("click", function() {
-    gridContainer.classList.toggle("no-guides");
-});
